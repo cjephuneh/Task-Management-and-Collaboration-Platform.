6dccdb11dfd5f1fdf489102bbb7b2c2b9878ccb1
cjephuneh <calebjephuneh@gmail.com>
2023-08-20 22:18:25 +0300

    rest_framework

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -5,10 +5,10 @@ class Project(models.Model):
     name = models.CharField(max_length=100)
     # Other fields...
 
-# class Task(models.Model):
-#     title = models.CharField(max_length=200)
-#     description = models.TextField()
-#     due_date = models.DateTimeField()
-#     assigned_to = models.ForeignKey(User, on_delete=models.CASCADE)
-#     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-#     # Other fields...
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    due_date = models.DateTimeField()
+    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    # Other fields...
