3e790393f0771967d1a011a4f937993bc797b7e4
cjephuneh <calebjephuneh@gmail.com>
2023-08-21 00:52:02 +0300

    auth

--- task_manager/Authentication/urls.py
+++ task_manager/Authentication/urls.py
@@ -1,10 +1,60 @@
-from django.urls import path
-from . import views
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model, authenticate, logout
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
-app_name = 'authentication'
+from .serializers import CustomUserSerializer
 
-urlpatterns = [
-    path('api/signup/', views.UserRegistrationAPIView.as_view(), name='api-signup'),
-    path('api/login/', views.UserLoginAPIView.as_view(), name='api-login'),
-    # Define URLs for profile management, logout, etc.
-]
+User = get_user_model()
+
+class UserRegistrationView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes = (permissions.AllowAny,)
+
+@api_view(['POST'])
+@permission_classes([permissions.AllowAny])
+def user_login(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    user = authenticate(username=username, password=password)
+    
+    if user:
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
+    
+    return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def user_logout(request):
+    request.auth.delete()  # Delete the token to logout
+    logout(request)
+    return Response({'message': 'Successfully logged out'})
+
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = CustomUserSerializer(request.user)
+        return Response(serializer.data)
+
+class PasswordChangeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        old_password = request.data.get('old_password')
+        new_password = request.data.get('new_password')
+
+        user = authenticate(username=request.user.username, password=old_password)
+
+        if user:
+            user.set_password(new_password)
+            user.save()
+            return Response({'message': 'Password changed successfully'})
+        else:
+            return Response({'error': 'Invalid old password'}, status=status.HTTP_400_BAD_REQUEST)
--- task_manager/Authentication/views.py
+++ task_manager/Authentication/views.py
@@ -1,38 +1,60 @@
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, redirect
-
-def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            return redirect('dashboard')  # Redirect to your dashboard URL
-    else:
-        form = UserCreationForm()
-    return render(request, 'signup.html', {'form': form})
-
-# You can implement similar views for login and profile management
-
-from django.contrib.auth import authenticate, login as auth_login
-from rest_framework.views import APIView
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model, authenticate, logout
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
-class UserLoginAPIView(APIView):
+from .serializers import CustomUserSerializer
+
+User = get_user_model()
+
+class UserRegistrationView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes = (permissions.AllowAny,)
+
+@api_view(['POST'])
+@permission_classes([permissions.AllowAny])
+def user_login(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    user = authenticate(username=username, password=password)
+    
+    if user:
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
+    
+    return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def user_logout(request):
+    request.auth.delete()  # Delete the token to logout
+    logout(request)
+    return Response({'message': 'Successfully logged out'})
+
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = CustomUserSerializer(request.user)
+        return Response(serializer.data)
+
+class PasswordChangeView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-        user = authenticate(username=username, password=password)
+        old_password = request.data.get('old_password')
+        new_password = request.data.get('new_password')
 
-        if user is not None:
-            auth_login(request, user)
-            token, created = Token.objects.get_or_create(user=user)
-            return Response({'token': token.key, 'user_id': user.id}, status=status.HTTP_200_OK)
+        user = authenticate(username=request.user.username, password=old_password)
+
+        if user:
+            user.set_password(new_password)
+            user.save()
+            return Response({'message': 'Password changed successfully'})
         else:
-            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+            return Response({'error': 'Invalid old password'}, status=status.HTTP_400_BAD_REQUEST)
