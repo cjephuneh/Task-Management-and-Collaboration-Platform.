c4d5e6a9c740865aa6e03f1a91aa3ad63d2ae57a
cjephuneh <calebjephuneh@gmail.com>
2023-08-20 21:41:23 +0300

    task_manager

---
+++ task_manager/tasks/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    # Other fields...
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    due_date = models.DateTimeField()
+    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    # Other fields...
