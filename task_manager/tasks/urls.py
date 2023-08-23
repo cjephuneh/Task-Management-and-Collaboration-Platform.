ec687d3f11c7f06b08e6b6d837e5ea945f86e53e
cjephuneh <calebjephuneh@gmail.com>
2023-08-23 17:56:34 +0300

    more_endpoints

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -1,12 +1,11 @@
-from .views import TaskViewSet
-from rest_framework import routers
+from django.urls import path
+from .views import TaskListAPIView, TaskDetailAPIView
+
+urlpatterns = [
+    path('api/tasks/', TaskListAPIView.as_view(), name='task-list'),
+    path('api/tasks/<int:pk>/', TaskDetailAPIView.as_view(), name='task-detail'),
+    # Add other URL patterns for other views (create, update, etc.)
+]
 
 
 
-app_name = 'tasks'
-
-router = routers.DefaultRouter()
-router.register('tasks', TaskViewSet, 'tasks' )
-
-
-urlpatterns = router.urls
--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-
-
+from rest_framework import generics
+from rest_framework.filters import DjangoFilterBackend, OrderingFilter
 from .models import TaskModel
 from .serializers import TaskSerializer
+
+
 
 # Create your views here.
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = TaskModel.objects.all()
     serializer_class = TaskSerializer
+
+
+class TaskListAPIView(generics.ListAPIView):
+    queryset = TaskModel.objects.all()
+    serializer_class = TaskSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]  # Enable filtering and sorting
+    filter_fields = ['status', 'due_date']  # Fields available for filtering
+    ordering_fields = ['created_at', 'due_date']  # Fields available for sorting
+
+
